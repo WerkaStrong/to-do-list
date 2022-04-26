a18e3ab5df3ba05efc20a1935b56065f1451fd18
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2022-04-26 18:40:02 +0200

    remove tasks

--- JS/script.js
+++ JS/script.js
@@ -10,20 +10,6 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
-                ${task.content}
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    }
-
     /* dodanie nowego zadania*/
     const addNewTask = (newTaskContent) => {
         tasks.push({
@@ -32,6 +18,41 @@
 
         render();
     };
+
+    /* usuwanie zadania*/
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+                render();
+    }
+
+    /* nadpisanie formularza w HTML */
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li
+                ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+                >
+                
+                <button class="js-remove">usuń</button>
+                ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    
+        /*wyciąganie wszystkich przycisków usuwania */
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        /* iteracja po każdym przycisku */
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    }
 
     const onFormSubmit = (event) => {
             event.preventDefault();
