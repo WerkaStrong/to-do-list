d1a36592dbadf56dbc1ad7e6675cae282fbc8ad3
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2022-08-01 20:57:26 +0200

    add immutability to function

--- JS/script.js
+++ JS/script.js
@@ -19,16 +19,33 @@
 
     /* usuwanie zadania*/
     const removeTask = (taskIndex) => {
-        tasks.splice(taskIndex, 1);
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            ...tasks.slice(taskIndex + 1),
+        ];
         render();
     };
 
     /* przekreślenie zadania - wpisuje przeciwnieństwo done*/
     const toogleTaskDone = (taskIndex) => {
-       /* tasks = tasks.map */
-        tasks[taskIndex].done = !tasks[taskIndex].done;
+        tasks = [
+            ...tasks.slice(0, taskIndex),
+            {
+                ...tasks[taskIndex],
+                done: !tasks[taskIndex].done,
+            },
+            ...tasks.slice(taskIndex + 1),
+        ]
         render();
     };
+
+    const markAllTasksDone = () => {
+        tasks = tasks.map((task) => ({
+            ...task,
+            done: true,
+        }));
+        render();
+    }
 
     const bindEvents = () => {
         /*wyciąganie wszystkich przycisków usuwania */
